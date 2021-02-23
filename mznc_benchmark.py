586f751682230f326255261bdcd787f3f7aa07b6
Jip J. Dekker <jip@dekker.one>
2021-02-24 10:36:04 +1100

    Add benchmark for the MiniZinc challenge

---
+++ mznc_benchmark.py
@@ -0,0 +1,21 @@
+from datetime import timedelta
+from pathlib import Path
+
+import minizinc
+
+from mzn_bench import Configuration, schedule
+
+schedule(
+    instances=Path("./mznc_instances.csv"),
+    timeout=timedelta(minutes=20),
+    configurations=[
+        Configuration("Gecode", solver=minizinc.Solver.lookup("gecode"), other_flags={"no-half-reifications": True, "no-chain-compression": True}),
+        Configuration("Gecode HR", solver=minizinc.Solver.lookup("gecode")),
+        Configuration("Gurobi", solver=minizinc.Solver.lookup("gecode"), other_flags={"no-half-reifications": True, "no-chain-compression": True}),
+        Configuration("Gurobi HR", solver=minizinc.Solver.lookup("gurobi")),
+        Configuration("CBC", solver=minizinc.Solver.lookup("cbc"), other_flags={"no-half-reifications": True, "no-chain-compression": True}),
+        Configuration("CBC HR", solver=minizinc.Solver.lookup("cbc")),
+    ],
+    nodelist=["critical001"],
+    output_dir=Path("./output/mznc/"),
+)
